efce305d273b79bb72bd04355b1595f29879549e
gilbert-linode <gilbert.liou.tw@gmail.com>
2014-11-05 16:20:14 +0000

    mv file to twcom folder

---
+++ insdb.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from twcom import makeindex
+from twcom import run_board_target as runbd
+
+makeindex.refresh()
+runbd.run_upd_boards()
+runbd.update_boss()
