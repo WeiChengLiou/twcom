3ad35a43c1802b5a9de3447060fc034f08e50d78
gilbert-akira <gilbert.liou.tw@gmail.com>
2014-11-21 18:10:39 +0800

    save REST API

--- api_server.py
+++ api_server.py
@@ -54,9 +54,16 @@ class BossNetwork(restful.Resource):
         return query.exp_boss(G)
 
 
+class Root(restful.Resource):
+    def get(self):
+        return 'ok'
+
+
 api.add_resource(ComNetwork, '/com')
 api.add_resource(BossNetwork, '/boss')
+api.add_resource(Root, '/')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+#app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
