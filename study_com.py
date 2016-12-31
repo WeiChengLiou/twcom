dd64f4d2142d27ac08324a0c322c2172cae22179
Wei Cheng Liou <gilbert.liou.tw@gmail.com>
2016-12-31 13:23:23 +0800

    Create company link table

--- study_com.py
+++ study_com.py
@@ -47,3 +47,54 @@ ret = (
             sep='\t', index=False)
 )
 ##
+# Create Company link
+coll = db.boards1
+ret = coll.aggregate([
+    {'$match': {'instid': {'$ne': np.nan}}},
+    {'$group': {'_id': {'instid': '$instid', 'id': '$id'},
+                'cnt': {'$sum': 1}}},
+    {'$sort': {'cnt': -1}},
+])
+ret = getdf(ret)
+ret = (
+    ret.join(pd.DataFrame(ret['_id'].to_dict()).T)
+    .drop('_id', axis=1)
+    .rename(columns={'instid': 'src', 'id': 'dst'})
+)
+
+board_cnt = coll.aggregate([
+    {'$match': {u'職稱': {'$ne': u'法人代表'},
+                'id': {'$in': ret['dst'].tolist()}}},
+    {'$group': {'_id': '$id', 'cnt': {'$sum': 1}}},
+    {'$sort': {'cnt': -1}},
+], allowDiskUse=True)
+board_cnt = (
+    getdf(board_cnt)
+    .rename(columns={'cnt': 'total', '_id': 'dst'}))
+
+ret = ret.merge(board_cnt, how='left')
+ret['seat_ratio'] = ret.cnt.astype('float') / ret.total
+ret = (
+    ret
+    .drop('total', axis=1)
+    .merge(iddic[['id', 'name']]
+           .rename(columns={'id': 'src', 'name': 'src_name'}))
+    .merge(iddic[['id', 'name']]
+           .rename(columns={'id': 'dst', 'name': 'dst_name'}))
+)
+
+##
+coll = db.ComLink1
+coll.drop()
+coll.insert_many(ret.to_dict('record'))
+print '%s inserted %s items' % (coll.name, coll.count())
+
+
+##
+# Study board list
+def get_boards(id):
+    ret = db.boards.find({'id': id})
+    return getdf(ret)
+
+
+##
