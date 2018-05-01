eca9e572356b4f753062665556696e1516b17918
Wei Cheng Liou <gilbert.liou.tw@gmail.com>
2018-05-01 21:25:24 +0800

    Fix proc_data

--- proc_data.sh
+++ proc_data.sh
@@ -2,4 +2,4 @@
 # python -c "from twcom.makeindex import insraw; insraw()"
 python -m proc_data.fix_badname
 python -m proc_data.study_com
-# python -m proc_data.fix_bossnode
+python -m proc_data.fix_bossnode
